3eb31afcec42c180fbe4d7e32194cd43b4fe696d
johannes-kuhfuss <johannes.kuhfuss@web.de>
2026-04-11 08:30:32 +0200

    added otel trace back

--- helper/Xcode.go
+++ helper/Xcode.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"log/slog"
@@ -12,6 +13,9 @@ import (
 	"github.com/johannes-kuhfuss/fileupload-service/config"
 	"github.com/johannes-kuhfuss/services_utils/logger"
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type XcodeRequest struct {
@@ -34,6 +38,17 @@ func StartXcode(cfg *config.AppConfig, filePath string) {
 		cfg.RunTime.OLog.Error(msg)
 		return
 	}
+	stc := trace.SpanContextFromContext(cfg.RunTime.Ctx)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ctx = trace.ContextWithRemoteSpanContext(ctx, stc)
+	tracer := otel.Tracer("fileupload-service")
+	ctx, span := tracer.Start(ctx, "transcode_request",
+		trace.WithAttributes(
+			attribute.String("http.url", xcodeUrl.String()),
+		),
+	)
+	defer span.End()
 
 	req.SourceFilePath = filePath
 	reqJson, err := json.Marshal(req)
@@ -41,16 +56,18 @@ func StartXcode(cfg *config.AppConfig, filePath string) {
 		msg := "Could not create transcode request"
 		logger.Error(msg, err)
 		cfg.RunTime.OLog.Error(msg, slog.String("Error Message", err.Error()))
+		span.RecordError(err)
 		return
 	}
 	hc := http.Client{
 		Transport: otelhttp.NewTransport(http.DefaultTransport),
 	}
-	hreq, err := http.NewRequest("POST", xcodeUrl.String(), bytes.NewBuffer(reqJson))
+	hreq, err := http.NewRequestWithContext(ctx, "POST", xcodeUrl.String(), bytes.NewBuffer(reqJson))
 	if err != nil {
 		msg := "Could not create transcode request"
 		logger.Error(msg, err)
 		cfg.RunTime.OLog.Error(msg, slog.String("Error Message", err.Error()))
+		span.RecordError(err)
 		return
 	}
 	hreq.Header.Add("Content-Type", "application/json")
@@ -59,6 +76,7 @@ func StartXcode(cfg *config.AppConfig, filePath string) {
 		msg := "Could not send transcode request"
 		logger.Error(msg, err)
 		cfg.RunTime.OLog.Error(msg, slog.String("Error Message", err.Error()))
+		span.RecordError(err)
 		return
 	}
 	defer resp.Body.Close()
